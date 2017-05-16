a68666e248b74186a0d9d1e9efb69f046d053b36
wellfrog <wellfrog16@gmail.com>
2017-05-17 02:31:02 +0800

    轮播

--- 响应式官网/js/lib/script.js
+++ 响应式官网/js/lib/script.js
@@ -4,7 +4,19 @@ define(['jquery', 'swiper', 'weixin'], function ($, swiper, wx) {
     var self = {}
 
     self.open = function () {
+        var mySwiper = new swiper('.swiper-container', {
+            loop: true,
+            autoplay: 2000,
+            paginationClickable: true,
+            autoplayDisableOnInteraction:false,
 
+            // 如果需要分页器
+            pagination: '.swiper-pagination',
+
+            // 如果需要前进后退按钮
+            nextButton: '.swiper-button-next',
+            prevButton: '.swiper-button-prev',
+        })
 
     }
 
