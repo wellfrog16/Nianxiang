df1960c5bb5f22a1d25047f9d0f92fdbdd4a5c3f
wellfrog <wellfrog16@gmail.com>
2017-05-16 21:04:53 +0800

    新建

---
+++ 响应式官网/js/lib/script.js
@@ -0,0 +1,33 @@
+// 剧本
+
+define(['jquery', 'swiper', 'weixin'], function ($, swiper, wx) {
+    var self = {}
+
+    self.open = function () {
+
+
+    }
+
+    return self;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
